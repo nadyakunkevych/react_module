8d58e8d743eb936d06ed1fdc33e62673f4f49dba
nadiakunkevic <nadyakunkevych@gmail.com>
2022-06-24 12:31:40 +0300

    hm1

--- src/components/Launch/Launch.js
+++ src/components/Launch/Launch.js
@@ -3,13 +3,13 @@ import css from '../launch.css'
 
 
 const Launch = (props) => {
-    const {mission_name, launch_year, image} = props;
+    const {mission_name, launch_year, links: {mission_patch_small}} = props;
 
     return (
         <div className="LaunchDiv">
             <div>MISSION NAME: {mission_name}</div>
             <div>YEAR: {launch_year}</div>
-            <img src={image} alt={'name'}/>
+            <img src={mission_patch_small} alt={'mission_name'}/>
         </div>
     );
 };
