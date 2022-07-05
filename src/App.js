da4163b79e39b4a772e07130968c738578f1f4a6
nadiakunkevic <nadyakunkevych@gmail.com>
2022-07-05 17:36:52 +0300

    hm1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import Users from "./components/users/Users";
-import Post from "./components/post/Post";
+// import Post from "./components/post/Post";
+import Posts from "./components/posts/Posts";
 import {getUsersPosts} from "./services/user.api.service";
 
 export default function App() {
@@ -9,10 +10,13 @@ export default function App() {
     const getPostbyUser = (id) => {
         getUsersPosts(id).then(({data}) => setPosts([...data]));
     }
+
     return (<div>
         <h2> All posts of user:</h2>
-        {posts.map(value => <Post key={value.id} item={value}/>)}
-        <br/>
+        {/*{posts.map(value => <Post key={value.id} item={value}/>)}*/}
+        {/*<br/>*/}
         <Users getPostbyUser={getPostbyUser}/>
+        <Posts posts={posts}/>
+
     </div>);
 }
